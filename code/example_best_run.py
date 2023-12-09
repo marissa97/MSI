f08e67484ce37a26fce6c4bdc6e5755bbdf54d63
marissa97 <marissa.eronika@gmail.com>
2023-12-09 11:05:23 +0100

    updates and results

---
+++ code/example_best_run.py
@@ -0,0 +1,63 @@
+from msi import *
+import numpy as np
+##### example ####
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+
+outDir= "/home/marissa/Documents/sysmex/MSI/results/example_best/"
+## create dummy test fasta file
+# shortage 5 "A"
+best_bat26="cccttaacctttttcaggtaaaaaaaaaaaaaaaaaaaagggttaaaaatgttgaatggttaaaaaatgtt"
+chunks1 = [best_bat26[i:i+20] for i in range(0, len(best_bat26), 20)]
+chunks2 = [best_bat26[i:i+20] for i in range(2, len(best_bat26), 20)]
+chunks3 = [best_bat26[i:i+20] for i in range(4, len(best_bat26), 20)]
+chunks4 = [best_bat26[i:i+20] for i in range(6, len(best_bat26), 20)]
+
+chunks= np.concatenate((chunks1, chunks2,chunks3,chunks4), axis=None)
+
+
+sequences=[]
+
+
+for i in range(0,len(chunks)):
+    record = SeqRecord(
+        Seq(chunks[i]),
+        id=str(i),
+        name="bat",
+        description="test"
+    )
+    print(record)
+    sequences.append(record)  # add code here
+
+SeqIO.write(sequences, outDir+"example.fasta", "fasta")
+
+## ref for bat26 fasta
+bat26_temp="cccttaacctttttcaggtaaaaaaaaaaaaaaaaaaaaaaaaaaagggttaaaaatgttgaatggttaaaaaatgtt"
+record = SeqRecord(
+        Seq(bat26_temp),
+        id=str(i),
+        name="referece",
+        description="reference"
+    )
+SeqIO.write(record, outDir+"bat26.fasta", "fasta")
+
+### parameter
+input_fastq_file = outDir+"example.fasta"
+bat26_temp="cccttaacctttttcaggtaaaaaaaaaaaaaaaaaaaaaaaaaaagggttaaaaatgttgaatggttaaaaaatgtt"
+bat26_start= 0
+bat26_end=78
+ad_start= bat26_start+20
+ad_end= bat26_start+ 46
+read_length=20
+
+
+## mapping 
+bamfile= mapFastatoBam("example", "bat26.fasta", outDir)
+
+## depth and cov calc for adenosin 
+dep, cov= bamReads(bamfile, ad_start, ad_end, len(bat26_temp),read_length, outDir)
+
+plotBar( ad_start, ad_end, dep, "Read Depths", "Adenosine Shortage Metrics (Depths)", "dep",outDir)
+plotBar( ad_start, ad_end, cov, "Coverage", "Adenosine Shortage Metrics (Coverage)", "cov",outDir)
+##
--- code/msi.py
+++ code/msi.py
@@ -1,14 +1,9 @@
 from Bio import SeqIO
 import subprocess
 import pysam
-
-input_fastq_file = "your_illumina_data.fastq"
-bat26_temp="cccttaacctttttcaggtaaaaaaaaaaaaaaaaaaaaaaaaaaagggttaaaaatgttgaatggttaaaaaatgtt"
-bat26_start= 100
-bat26_end=178
-ad_start= bat26_start+19
-ad_end= bat26_start+ 46
-## 1. read fastq
+import numpy as np
+import matplotlib.pyplot as plt 
+## 1. read fastq (QC)
 
 ## 2. map fastq to reference genome -> bam/sam file  or map to bat26_temp
 ## illumina limitation: 
@@ -16,75 +11,81 @@ ad_end= bat26_start+ 46
 ## - read length of NGS platforms is short (50 to 300), each position may have different coverage  
 ## - a read might not cover all the adenine region (depends on the length as well), 
 
-# Index the reference genome
-#subprocess.run(["samtools", "faidx", "reference_genome.fasta"])
-
-# Map reads to the reference genome
-#subprocess.run(["bwa", "mem", "reference_genome.fasta", "reads.fastq"], stdout=open("mapped_reads.sam", "w"))
-
-# Convert SAM to BAM
-#subprocess.run(["samtools", "view", "-bS", "mapped_reads.sam", "-o", "mapped_reads.bam"])
-
-# Sort the BAM file
-#subprocess.run(["samtools", "sort", "mapped_reads.bam", "-o", "sorted_mapped_reads.bam"])
-
-# Index the sorted BAM file
-#subprocess.run(["samtools", "index", "sorted_mapped_reads.bam"])
+##TODO: not specific for bat26, (this is for reads < 70bp)
+def mapFastatoBam(name, refname, outDir):
+    print(["bwa", "aln",outDir+refname, outDir+name+".fasta"])
+    # Index the reference genome
+    #gatk-launch CreateSequenceDictionary -R [genomeRefFasta] -> for vcf
+    subprocess.run(["samtools", "faidx", outDir+refname])
+    subprocess.run(["bwa", "index", outDir+refname])
+    # Map reads to the reference genome
+    subprocess.run(["bwa", "aln",outDir+refname, outDir+name+".fasta"], stdout=open(outDir+name+".sai", "w"))
+    subprocess.run(["bwa", "samse",outDir+refname, outDir+name+".sai", outDir+name+".fasta"], stdout=open(outDir+"mapped_reads.sam", "w"))
+    # Convert SAM to BAM
+    subprocess.run(["samtools", "view", "-bS", outDir+"mapped_reads.sam", "-o", outDir+"mapped_reads.bam"])
+    # Sort the BAM file
+    subprocess.run(["samtools", "sort", outDir+"mapped_reads.bam", "-o", outDir+name+".bam"])
+    # Index the sorted BAM file
+    subprocess.run(["samtools", "index", outDir+name+".bam"])
+    return outDir+name+".bam"
 
 ##3. select region
 ## if map to human genome -> extracting the sam file to bat26 region 
 #(chr: bam_file.get_reference_name(read.reference_id), start: read.reference_start, end)
 
-bat26_bam = 'bat26.bam'
 
-##4. Calculate depth to prove MSI 
+
+
+##4. Calculate depth and coverage to prove MSI 
 ## depth can be calculated with samtools: samtools depth [options] [in1.sam|in1.bam|in1.cram [in2.sam|in2.bam|in2.cram] [...]] 
 ## limitation: if not 'a' (mismatch)?
-
+##TODO: usage of cigar string, if reads may be splitted (?)
 ## if not: 
-def bamReads(bamfile,seq, ad_st, ad_end, reflength,read_length):
+def bamReads(bamfile, ad_st, ad_end, reflength,read_length, outDir):
     len_ad= ad_end-ad_st
     dep_arr=np.zeros(len_ad)
     # Open the BAM file
-    with pysam.AlignmentFile(bamfile, 'rb') as bamfile:
+    with pysam.AlignmentFile(bamfile, 'rb') as bf:
         # Iterate over reads in the BAM file
-        for read in bamfile.fetch():
-            # Get the read name and sequence
+        #print(len(bf.fetch()))
+        for read in bf.fetch():
             read_name = read.query_name
             sequence = read.query_sequence
-            start_position = read.reference_start
-            end_position = read.reference_end
-
-            ## if shortening in adenine -> msi detection ?
-            ## using covarage: 
+            positions= read.positions
             # illumina limitation: if reads < adenosine length (e.g. aaaa, this can be mapped to any position in the adenosin repeats)
-            
-            dep_arr= dep_arr+ calcReads(len_ad,ad_st, ad_end, start_position,end_position, sequence)
-    cov_arr= calcCoverage(dep_arr, read_length, reflength)
-
+            dep_arr= dep_arr+ calcReads(len_ad,ad_st, ad_end, positions, sequence)
+    cov_arr= calcCoverage(dep_arr, read_length, reflength) 
     return dep_arr,cov_arr
                 
 
-    
-
-
-def calcReads(len_ad,ad_st, ad_end, start_position,end_position, sequence):
+#calculate depth
+def calcReads(len_ad,ad_st, ad_end, positions, sequence):
     dep_arr=np.zeros(len_ad)
-    if ad_st<=start_position<=ad_end and end_position<=ad_end:
-        ad= sequence[ad_st:ad_end]
-        for i in range(0,len(ad)):
-            if ad[i]=="a":
-                dep_arr[i]=cov_arr[i]+1
+    positions= np.array(positions)
+    positions= positions[np.where((positions >= ad_st) & (positions <= ad_end))]
+    sequence=  np.array(list(sequence))
+    sequence= sequence[np.where((positions >= ad_st) & (positions <= ad_end))]
+    if len(positions)!=0:
+        for i in range(0, len(positions)):
+            print(i)
+            print(positions[i])
+            print(sequence[i])
+            if sequence[i]=="A":
+                print("jo")
+                dep_arr[positions[i]-ad_st]=dep_arr[positions[i]-ad_st]+1
     return dep_arr
 
 
-## Another approach is calculating coverage: per position: (number of reads) x (length of each read) / (length of the reference)
+## calculating coverage: per position: (number of reads) x (length of each read) / (length of the reference)
 ## e.g. if in the position coverage <10% -> shortened
-
 def calcCoverage(dep_arr, read_length, reflength):
     cov_arr=np.zeros(len(dep_arr))
     for i in range(0,len(dep_arr)):
-        cov_arr[i]= dep_arr[i] * read_length/reflength
+        print(dep_arr[i] * read_length / reflength)
+        cov_arr[i]= dep_arr[i] * read_length / reflength
+    print(cov_arr)
+    return cov_arr
+
 
 
 ## 5. Decision making, definition of adenosin region that is shortened
@@ -94,9 +95,24 @@ def calcCoverage(dep_arr, read_length, reflength):
 #- how does the distribution of the depth looked like?
 #- mean depth of all region? mean depth of the adenosin region?
 # e.g. depth: decision: region hast to have mind 5 reads
-
-
-
 ## e.g. coverage: decision:if in the position coverage <10% -> shortened
+def plotBar( ad_st, ad_end, yval, ylab, title,name,outDir):
+    xval= list(range(ad_st, ad_end))
+    yval = list(yval)
+    fig = plt.figure(figsize = (10, 5))
+    # creating the bar plot
+    plt.bar(xval, yval, color ='maroon', 
+        width = 0.4)
+    plt.xlabel("Adenosin Positions")
+    plt.ylabel(ylab)
+    plt.title(title)
+    #plt.show()
+    plt.savefig(outDir+name)
+
+
 
 ### illumina limitations: noise
+
+
+
+
